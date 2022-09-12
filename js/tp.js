6eb1f301b9daff1ab7928b44f9a8103e52760ce0
jean konan <shagguyyoung@gmail.com>
2022-09-12 22:56:22 +0000

    version 2

--- js/tp.js
+++ js/tp.js
@@ -1,11 +1,1 @@
-let numberOfSeasons = 6;
-let numberOfEpisodes = 12;
-
-
-let episodeTime = 45;
-let commercialTime = 5;
-totalShowTime = (episodeTme + commecialTime) * numberOfSeasons + numberOfEpisodes;
-let paragraph = document.querySelector('#info');
-paragraph.innerText = ${numberOfSeasons} seasons,${numberOfEpisodes} episode per seasons
-
-Total viewing time: ${totalShowTime} minutes
+prompt('quel est votre nom?');
